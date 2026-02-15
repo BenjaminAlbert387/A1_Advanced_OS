fbe60b0f89786a5b9aa6334239d0f21e420f0845
Benjamin <BA387@canterbury.ac.uk>
2026-02-15 11:57:48 +0000

    save

--- task1.sh
+++ task1.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Hello, World!"
+
+# Use bash {scriptname}.sh to run
+
+# Get the directory where this script is currently located
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to that directory if needed
+cd "$BASE_DIR" || {
+    echo "Failed to change directory to $BASE_DIR"
+    exit 1
+}
+
+# Output working directory 
+echo "Now working in: $(pwd)"
+
+# Initialise files
+BACKUP_DIR="$BASE_DIR/Backup"
+LOG_FILE="$BASE_DIR/backup_log.txt"
+WARNING_LIMIT_MB=50
