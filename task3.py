517bcd72e1bb7816ce2d6ab33210df5895fc10c0
Benjamin <BA387@canterbury.ac.uk>
2026-02-21 14:17:22 +0000

    improved menu logic

--- task3.py
+++ task3.py
@@ -42,20 +42,39 @@ def create_submitted_assigments_py_directory_function():
         menu_function()
 
 def submit_assigment_function():
+    if os.path.isdir("Submitted_Assigments_Py") == False:
+        print("Error: Please create Submitted_Assigments_Py first!")
+        menu_function()
+
     # User inputs the file name
     file = input("Type the file name, including the .pdf part, and press Enter: ")
 
     # Variable that stores the path of the base directory and file name inputted
     check_file = os.path.join(BASE_DIR, file)
 
+    # Variable that stores the extension of the file name inputted
+    ext_file = os.path.splitext(file)[-1].lower()
+
+    # Variable that stores the size of the file in bytes
+    # To convert bytes to MB, times by 1,000,000
+    size_file = os.path.getsize(file)
+
     # Checks to see whether the file exists in the base directory 
     if os.path.exists(check_file) == True:
         print("File exists in the base directory")
 
+        # Input validation 1: Check for matching file names
         for files in os.listdir("Submitted_Assigments_Py"):
             if fnmatch.fnmatch(file, files):
                 print("Error: File with the same name has already been submitted")
 
+        # Input validation 2: Check for supported file types
+        if ext_file != ".docx" or ext_file != ".pdf":
+            print("Error: File type not supported")
+
+        # Input validation 3: Check if the file is over 5MB
+
+        
     else:
         print("Error: file does not exist in base directory")
 
@@ -70,17 +89,20 @@ def menu_function():
     choice = int(input("Type in a valid integer from the main menu: "))
     print("=======================================================================================")
 
-    if choice != 1 and choice != 2 and choice != 3 and choice != 4 and choice != 5:
-        print("Error: invalid choice")
-        menu_function()
-    
-    else:
-        print("Valid option")
+    while True:
+        try:
+            choice = int(input("Enter a number: "))
+            break
+        except ValueError:
+            print("Error: Invalid input!")
 
         if choice == 1:
             create_submitted_assigments_py_directory_function()
 
-        if choice == 2:
+        elif choice == 2:
             submit_assigment_function()
+
+        else:
+            print("Error: not a valid menu choice")
 
 menu_function()
