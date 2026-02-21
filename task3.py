0ae6e37f0ef76bc9095505b635b38bb63c56e46c
Benjamin <BA387@canterbury.ac.uk>
2026-02-21 13:24:56 +0000

    save

--- task3.py
+++ task3.py
@@ -1,8 +1,24 @@
 # Use python {path to the file}/task.py in powershell terminal, then press Enter to run
 # For example: python C:\Users\benja\Documents\GitHub\A1_Advanced_OS\task3.py
 
+# Initialisation: import classes when the program is run
+import os
+import sys
+
+# Get the directory where this script is located
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Change to that directory
+try:
+    os.chdir(BASE_DIR)
+except Exception as e:
+    print(f"Failed to change directory to {BASE_DIR}: {e}")
+    sys.exit(1)
+
+# Output working directory
+print("Now working in:", os.getcwd())
+
 def create_submitted_assigments_py_directory_function():
-        import os
 
         # Specify the directory name
         directory_name = "Submitted_Assigments_Py"
@@ -10,7 +26,11 @@ def create_submitted_assigments_py_directory_function():
         # Create the directory
         try:
             os.mkdir(directory_name)
+
+            # Ouptut success message to the user
             print(f"Directory '{directory_name}' created successfully.")
+        
+        # Python class that is true if the directory already exists
         except FileExistsError:
             print(f"Directory '{directory_name}' already exists.")
         except PermissionError:
@@ -19,6 +39,15 @@ def create_submitted_assigments_py_directory_function():
             print(f"An error occurred: {e}")
 
         menu_function()
+
+def submit_assigment_function():
+    file = input("Type the file name, including the .pdf part, and press Enter:")
+    check_file = BASE_DIR/file
+    if check_file == True:
+        print("File exists in the base directory")
+
+    else:
+        print("Error: file does not exist in base directory")
 
 def menu_function():
     print("=======================================================================================")
