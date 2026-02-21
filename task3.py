6373922c0cc69c73bce4f1f412643c3079de92e4
Benjamin <BA387@canterbury.ac.uk>
2026-02-21 12:41:35 +0000

    begun task 3 python

---
+++ task3.py
@@ -0,0 +1,11 @@
+def menu_function():
+    print("=======================================================================================")
+    print("University Examination Board Main Menu:")
+    print("1: Create Submitted_Assignments Directory")
+    print("2: Submit Assignment")
+    print("3: Check Submitted Files")
+    print("4: Check Submitted_Assignments Directory")
+    print("5: Exit")
+    print("=======================================================================================")
+
+menu_function()
